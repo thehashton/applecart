efb758c751b06598ad5e507103ceca73f930dcd1
thehashton <harry.ashton.uk@gmail.com>
2024-08-10 00:12:02 +0100

    updates vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     server: {
         fs: {
-            strict: false, // This ensures that static files outside of Vite's root directory can be served
+            strict: true,
         },
     },
 });
