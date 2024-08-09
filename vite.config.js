788c718e2a4c612afa6c9d4a91b30a5ea085551f
thehashton <harry.ashton.uk@gmail.com>
2024-08-10 00:30:38 +0100

    Gets vite to build js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     build: {
         outDir: 'dist',
+        rollupOptions: {
+            input: './index.html',
+        },
     },
     server: {
         fs: {
